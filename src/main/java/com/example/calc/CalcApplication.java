89ff836affa79daca291769a0637eb5000b032d6
Voodoo <design.serg@gmail.com>
2024-10-19 20:21:02 +0300

    Add icon

--- src/main/java/com/example/calc/CalcApplication.java
+++ src/main/java/com/example/calc/CalcApplication.java
@@ -3,6 +3,7 @@ package com.example.calc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,6 +14,8 @@ public class CalcApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalcApplication.class.getResource("calc-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 470);
+        Image image = new Image("icon.png");
+        stage.getIcons().add(image);
         stage.setTitle("Calc");
         stage.setScene(scene);
         stage.show();
